2771ca15040f826a60a39a1064447bd417f20f5d
Haselnussbomber <mail@haselnussbomber.de>
2026-06-13 05:02:35 +0200

    Add select sqpack directory button

--- SqPackFs/Components/SettingsCard.cs
+++ SqPackFs/Components/SettingsCard.cs
@@ -1,6 +1,7 @@
 using Microsoft.UI.Reactor;
 using Microsoft.UI.Reactor.Core;
 using SqPackFs.Services;
+using Windows.Storage.Pickers;
 using static Microsoft.UI.Reactor.Factories;
 
 namespace SqPackFs.Components;
@@ -12,13 +13,23 @@ public class SettingsCard : Component
         var gameDataProvider = UseObservable(UseMemo(ServiceLocator.GetService<GameDataProvider>));
         var pathlist = UseObservable(UseMemo(ServiceLocator.GetService<PathList>));
 
+        var selectDirectoryCallback = UseCallback(async () =>
+        {
+            var folder = await UseFolderPickerAsync(new FolderPickerOptions(PickerLocationId.ComputerFolder)); // TODO: fix warning
+            if (folder == null || !Directory.Exists(folder?.Path))
+                return;
+
+            ServiceLocator.GetService<GameDataProvider>().SetGamePath(folder.Path);
+        });
+
         return Card(
             VStack(
                 SubHeading("Settings")
                     .Margin(bottom: 12),
                 TextBox(gameDataProvider.GamePath, gameDataProvider.SetGamePath, string.Empty, "Path to sqpack directory")
                     .AutomationName("GamePath")
-                    .IsEnabled(pathlist.Status is not (PathListStatus.Downloading or PathListStatus.Loading))
+                    .IsEnabled(pathlist.Status is not (PathListStatus.Downloading or PathListStatus.Loading)),
+                Button("Select sqpack directory", selectDirectoryCallback)
             )
         )
         .Margin(bottom: 16);
